bd77a9c14f460e9c83cb9bc7e0b9248bc13242c2
BunnyBlack <xyy6836@126.com>
2020-12-30 17:58:13 +0800

    variabletest

---
+++ Assets/Scripts/VariableTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class VariableTest : MonoBehaviour
+    {
+        private int _score;
+
+        private void Start()
+        {
+            _score = 100;
+            Debug.Log(_score.ToString());
+        }
+    }
+}
